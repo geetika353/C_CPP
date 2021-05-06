e83cf4f68a6073a769751564dcd06a552c09ef38
GEETIKA JANA <geetika353@gmail.com>
2021-05-06 09:58:38 +0530

    SecondCommit

--- PassingArrayToFunc.cpp
+++ PassingArrayToFunc.cpp
@@ -7,4 +7,19 @@ int main()
     int A1[5]={10,20,30,40,50};
 
     int A2[5]={5,15,20,25,30};
+
+    printArray(a1);
+    printArray(a2);
+}
+
+
+void printArray(int a[5])
+{
+
+    cout<<"Printing the array elements:"<<endl;
+
+    for(int i=0;i<5;i++)
+    { 
+        cout<<a[i];<<'\n';
+    }
 }
