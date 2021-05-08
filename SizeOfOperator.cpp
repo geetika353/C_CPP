3fd7e2450c356a2d50866d1a098d4e1efaefd994
GEETIKA JANA <geetika353@gmail.com>
2021-05-08 21:01:59 +0530

    FirstCommit

---
+++ SizeOfOperator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout<<"Size of Integer Data Type: "<<sizeof(int)<<endl;
+     cout<<"Size of Float Data Type: "<<sizeof(float)<<endl;
+      cout<<"Size of Double Data Type: "<<sizeof(double)<<endl;
+       cout<<"Size of Character Data Type: "<<sizeof(char)<<endl;
+
+       return 0;
+}
