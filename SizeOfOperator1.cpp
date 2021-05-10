9f071afd8e9cfe1c469d4ce468af364785636b5b
GEETIKA JANA <geetika353@gmail.com>
2021-05-10 11:13:44 +0530

    FirstCommit

---
+++ SizeOfOperator1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+
+    int A;
+
+};
+
+int main()
+{
+    Base B;
+    cout<<"Size of class Base is:"<<sizeof(B)<<endl;
+
+    return 0;
+}
