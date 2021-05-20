21a946cbf3fd561901c4ea4856141d45789fce5d
GEETIKA JANA <geetika353@gmail.com>
2021-05-20 07:22:41 +0530

    FirstCommit

---
+++ FuncPtr1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void printname(char *name)
+{
+    cout<<"Name is: "<<name<<endl;
+}
+
+int main()
+{
+    char s[20];// array declaration
+
+    void(*ptr)(char *);// function ptr declaration
+    ptr= printname;// storing the address of the printname
+
+    cout<<"Enter the name of the person: "<<endl;
+    cin>>s;
+
+    cout<<s<<endl;
+
+    ptr (s);//calling printname() function
+    return 0;
+}
