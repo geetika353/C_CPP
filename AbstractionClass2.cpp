c9cef597d083e2dd02d9983750c480299c55b3a4
GEETIKA JANA <geetika353@gmail.com>
2021-05-24 00:05:04 +0530

    FirstCommit

---
+++ AbstractionClass2.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+# include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int n=5;
+    int power=3;
+
+    int result= pow(n, power); // pow(n, power) is the power function
+
+    cout<<"Cube of n is: "<<result<<endl;
+
+    return 0;
+
+
+}
