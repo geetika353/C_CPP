0f69438efee9c943b1971eefd477fa17f591eeb8
GEETIKA JANA <geetika353@gmail.com>
2021-06-01 00:47:02 +0530

    FirstCommit

---
+++ C++Programming9.cpp
@@ -0,0 +1,19 @@
+/* Program to swap two numbers without using third Variable by '+' and '-' */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10, b = 50;
+
+    cout<<"Before swap a = "<<a <<" and b = "<< b<<endl;
+
+    a = a*b;
+    b = a/b;
+    a = a/b;
+
+    cout<<"After swap a = "<<a<<" and b = "<<b<<endl;
+
+    return 0;
+}
