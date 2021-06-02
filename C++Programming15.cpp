e1d1c4829c1c6887ea06993629faff73a3e1efe9
GEETIKA JANA <geetika353@gmail.com>
2021-06-03 00:05:41 +0530

    FirstCommit

---
+++ C++Programming15.cpp
@@ -0,0 +1,31 @@
+/* Program to print alphabet  triangle */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch = 'A';
+
+    int i, j, k, m;
+
+    for(i =1; i<=5; i++)
+    {
+        for(j=5; j>=1; j--)
+        cout<<"   ";
+
+        for(k=1; k<=i; k++)
+        cout<<ch++;
+
+        ch--;
+
+        for(m=1;m<i ; m++)
+        cout<<--ch;
+
+        cout<<"\n";
+
+        ch='A';
+    }
+
+    return 0;
+}
