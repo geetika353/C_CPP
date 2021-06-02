4f51f0d5028bfdf12ce085326dbd59a9688b1715
GEETIKA JANA <geetika353@gmail.com>
2021-06-02 23:49:55 +0530

    FirstCommit

---
+++ C++Programming14.cpp
@@ -0,0 +1,96 @@
+/* Program to convert number into characters */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    long int n, sum=0, r;
+
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    while(n>0)
+    {
+        r = n % 10;
+
+        sum = sum *10 + r;
+        n= n/10;
+    }
+
+    n= sum;
+
+    while (n>0)
+    {
+
+        r= n% 10;
+
+        switch(r)
+        {
+            case 1:
+
+            cout<<"One "<<"   ";
+
+            break;
+
+            case 2:
+
+            cout<<"Two "<<"   ";
+
+            break;
+
+            case 3:
+
+            cout<<"Three "<<"   ";
+
+            break;
+
+            case 4:
+
+            cout<<"Four"<<"   ";
+
+            break;
+
+            case 5:
+
+            cout<<"Five"<<"   ";
+
+            break;
+
+            case 6:
+
+            cout<<"Six"<<"   ";
+
+            break;
+
+            case 7:
+
+            cout<<"Seven"<<"   ";
+
+            break;
+
+            case 8:
+
+            cout<<"Eight"<<"   ";
+
+            break;
+
+            case 9:
+
+            cout<<"Nine "<<"   ";
+
+            break;
+
+            case 0:
+
+            cout<<"Zero"<<"   ";
+
+            break;
+
+
+        }
+
+        n= n/10;
+    }
+}
